27a69ba8905e6d245a317ecedc61667dabe321c6
Angel Heaven <quentin.deregnaucourt@gmail.com>
2018-01-23 13:10:13 +0100

    initial project creation

---
+++ app/src/main/java/com/angel/heaven/moodtracker/MoodChoiceActivity.java
@@ -0,0 +1,13 @@
+package com.angel.heaven.moodtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoodChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_choice);
+    }
+}
